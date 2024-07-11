4abd7e00637a39d71a73c901727142cdee6aa8e4
Kanae <kanae.bunche@gmail.com>
2024-07-11 14:44:18 -0400

    students

--- E-Commerence/script.js
+++ E-Commerence/script.js
@@ -2,19 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     //Step 1: Define an array of products (object)
 
     const products =[
-        {id: 1 , name: 'Red T-Shirt', price: 20, img: 'Images.png'},
-        {id: 2 , name : 'Purple T-Shirt', price: 20, img: 'Images.png'},
-        {id: 3 , name: 'Orange T-Shirt', price: 20, img: 'Orange.png'},
-        {id: 4 , name : 'Brown T-Shirt', price: 20, img: 'Images.png'},
-        {id: 5 , name: 'Yellow T-Shirt', price: 20, img: 'Images.png'},
-        {id: 6 , name : 'Pink T-Shirt', price: 20, img: 'Images.png'},
-        {id: 7 , name: 'Blur T-Shirt', price: 20, img: 'Images.png'},
-        {id: 8 , name : 'Black T-Shirt', price: 20, img: 'Images.png'},
-        {id: 9 , name: 'White T-Shirt', price: 20, img: 'Images.png'},
-        {id: 10 , name : 'Red-Orange T-Shirt', price: 20, img:'Images.png'},
+        {id: 1 , name: 'Red T-Shirt', price: 20, img:"Images/red-t-shirt.jpeg"},
+        {id: 2 , name : 'Red T-Shirt', price: 20, img: 'Images/red-t-shirt.jpeg'},
+        {id: 3 , name: 'Red T-Shirt', price: 10, img: 'Images/red-t-shirt.jpeg'},
+        {id: 4 , name : 'Red T-Shirt', price: 20, img: 'Images/red-t-shirt.jpeg'},
+        {id: 5 , name: 'Red T-Shirt', price: 20, img: 'Images/red-t-shirt.jpeg'},
+        {id: 6 , name : 'Red T-Shirt', price: 20, img: 'Images/red-t-shirt.jpeg'},
+        {id: 7 , name: 'Red T-Shirt', price: 20, img: 'Images/red-t-shirt.jpeg'},
+        {id: 8 , name : 'Red T-Shirt', price: 20, img: 'Images/red-t-shirt.jpeg'},
+        {id: 9 , name: 'Red T-Shirt', price: 20, img: 'Images/red-t-shirt.jpeg'},
+        {id: 10 , name : 'Red T-Shirt', price: 20, img:'Images/red-t-shirt.jpeg'},
  ]
  
-});
+
 
 // Step 2 : Initialize cart and whishlist counts
 let cartCount = 0;
@@ -23,8 +23,32 @@ let heartCount = 0;
 // Step 3 : Get DOM elements
 const searchInput = document.getElementById("search");
 const sortPriceButton = document.getElementById("sort-price");
-const heartNumber = document.getquerySelector(".heart-number");
-const cartNumber = document.getquerySelector(".cart-number");
+const heartNumber = document.querySelector(".heart-number");
+const cartNumber = document.querySelector(".cart-number");
 const productContainer = document.getElementById("product-list");
 
 
+//Step 4: Function to display products 
+function displayProducts(productsToShow){
+    productContainer.innerHtml = " ";
+// Loop through the products
+productsToShow.forEach(product => {
+ const productDiv = document.createElement('div');
+ productDiv.classList.add('product');
+ productDiv.innerHTML =`
+   <img src="${product.img}" alt="${product.name}">
+   <h2> ${product.name}</h2>
+   <p class="price">$${product.price}</p>
+   <button class="add-cart" data-id="${product.id}">Add to Cart</button>  
+   <button class="add-heart" data-id="${product.id}">Add to Wishlist</button>
+`;
+
+// Append products 
+productContainer.appendChild(productDiv);
+})
+
+}
+
+displayProducts(products);
+
+});
